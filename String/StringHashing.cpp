95f04aedf085a89d07b04ef903d89f747e8876fe
M-Manas-s <manastiwari111222@gmail.com>
2021-08-03 11:39:34 +0530

    String Hashing

--- String/StringHashing.cpp
+++ String/StringHashing.cpp
@@ -4,31 +4,7 @@
 using namespace std;
 using namespace __gnu_pbds;
 
-#define vi vector<int>
-#define vll vector<ll>
-#define vvi vector < vi >
-#define vpii vector < pair <int,int> >
-#define pb(x) push_back(x)
-#define pii pair<int,int>
-#define pll pair<long long, long long>
-#define all(c) c.begin(),c.end()
-#define mp(x,y) make_pair(x,y)
-#define mem(a,val) memset(a,val,sizeof(a))
-#define eb emplace_back
-#define ff first
-#define ss second
-#define lc(p) (p << 1)
-#define rc(p) (p << 1) | 1
-#define ps(x, y) fixed << setprecision(y) << x
-#define mk(arr, n, type) type *arr = new type[n]
-#define range(a, b) substr(a, b - a + 1)
-#define trace(x) cerr << #x << ": " << x << endl
-#define FIO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
-#define input(arr,n) FOR(i,0,n) cin>>a[i]
 #define FOR(i,k,n) for ( int i=k; i<n; i++ )
-#define ROF(i,k,n) for ( int i=k; i>n; i-- )
-#define ll long long
-#define casePrint(x,y) cout<<"Case #"<<x<<": "<<y;
 
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 
@@ -127,12 +103,12 @@ int H[N];
  
 void precalc() {
     pw[0] = 1;
-    for(int i = 1; i < N; i++)
+    FOR(i,1,N)
         pw[i] = mult(pw[i - 1], base, mod);
     
     int pw_inv = power(base , mod - 2 , mod);
     inv[0] = 1;
-    for(int i = 1; i < N; i++)
+    FOR(i,1,N)
         inv[i] = mult(inv[i - 1], pw_inv, mod);
 }
 
@@ -150,7 +126,7 @@ void precalc() {
  
 void build(string s){
     int n = s.length();
-    for(int i = 0; i < n ; ++i){
+    FOR(i,0,n){
         H[i] = add((i == 0) ? 0 : H[i - 1], mult(pw[i], s[i] - 'a' + 1, mod), mod);
     }
 }
